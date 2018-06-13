325c4c90d003adbef00a13ecbb84bb4379b7eaf1
Walter Kruml <walter@opaque.at>
2018-06-13 09:32:44 +0200

    geändert:   Classes/ViewHelpers/GetCategoriesViewHelper.php 	Classes/ViewHelpers/IsCategorySelectedViewHelper.php

--- Classes/ViewHelpers/GetCategoriesViewHelper.php
+++ Classes/ViewHelpers/GetCategoriesViewHelper.php
@@ -4,8 +4,20 @@ namespace OD\RegisteraddressCategories\ViewHelpers;
 
 class GetCategoriesViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
 
+    /**
+     * @return void
+     */
+    public function initializeArguments() {
+        $this->registerArgument('table', 'string', 'The table of the record.', false, 'tt_content');
+        $this->registerArgument('uid', 'int', 'The uid of the record.', true);
+    }
+ 
+    /**
+     * @return array|bool
+     */
     public function render() {
-        return 'World';
+        $result = BackendUtility::getRecord($this->arguments['table'], $this->arguments['uid']);
+        return (empty($result) === false) ? $result : false;
     }
 
 }
